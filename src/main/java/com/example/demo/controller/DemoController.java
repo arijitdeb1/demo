1f8ba6861e281801e53d8819658166a096febc2e
Arijit Deb <adeb@deloitte.com>
2019-12-28 09:39:23 +0530

    commit 2

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -11,4 +11,9 @@ public class DemoController {
     public String test1(){
         return "test1";
     }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
+    }
 }
